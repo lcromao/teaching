11781d5e824186553d10adf9ce7cdf18fc3abed2
Lucas Romao <lc.romao98@gmail.com>
2025-11-19 07:30:50 -0300

    refactor: Simplify logging output format in scripts.sh

--- todo-list/server/scripts.sh
+++ todo-list/server/scripts.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 
-# Colors for output
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
-
 # Logging helpers
 print_status() {
-    echo -e "${BLUE}[INFO]${NC} $1"
+    echo -e "$[INFO]$ $1"
 }
 
 print_success() {
-    echo -e "${GREEN}[SUCCESS]${NC} $1"
+    echo -e "$[SUCCESS]$ $1"
 }
 
 print_warning() {
-    echo -e "${YELLOW}[WARNING]${NC} $1"
+    echo -e "$[WARNING]$ $1"
 }
 
 print_error() {
-    echo -e "${RED}[ERROR]${NC} $1"
+    echo -e "$[ERROR]$ $1"
 }
 
 # Virtualenv helpers
