04a313c92eec0bc4f4ce3ed17c7efc3b5d568c1b
Lucas Romao <lc.romao98@gmail.com>
2025-11-28 19:06:57 -0300

    feat: implement user login functionality with form and API integration

--- todo-list/client/scripts.js
+++ todo-list/client/scripts.js
@@ -21,3 +21,28 @@ const postUser = async () => {
 			console.error("Error:", error);
 		});
 };
+
+const loginUser = async () => {
+	let email = document.getElementById("loginEmail").value;
+	let password = document.getElementById("loginPassword").value;
+	payload = {
+		email: email,
+		password: password,
+	};
+
+	let url = "http://127.0.0.1:8000/auth/login";
+	fetch(url, {
+		method: "post",
+		body: JSON.stringify(payload),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			localStorage.setItem("token", data.access_token);
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+		});
+};
